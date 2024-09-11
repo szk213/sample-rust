e26756cd0039ad9fb640ec8ff2d0e5c5f792841e
szk213 <18608225+szk213@users.noreply.github.com>
2024-09-11 11:02:41 +0000

    add:CopySemantics

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,12 @@ fn main() {
     let mut x = 1;
     x += rng.gen::<i32>();
     let url = format!(base_url!(), key = "sample_key", page = x);
-    println!("{}", url)
+    println!("{}", url);
+
+    // Copy Semantics
+    let s = 1;
+    let t = s;
+
+    println!("{}", t);
+    println!("{}", s);
 }
