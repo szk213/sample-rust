db1cf109f9f3697db409ff139d946b8451c13bd9
szk213 <18608225+szk213@users.noreply.github.com>
2024-09-11 11:00:39 +0000

    add:lint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rand::Rng;
+
+macro_rules! base_url {
+    () => {
+        "http://sample.com/search?key={key}&page={page}"
+    };
+}
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let mut x = 1;
+    x += rng.gen::<i32>();
+    let url = format!(base_url!(), key = "sample_key", page = x);
+    println!("{}", url)
 }
